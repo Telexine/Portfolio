cfc56f17c9f4310a70cc7df53bd7477dc7549b60
Telexine <tanuki360@hotmail.com>
2019-08-07 21:40:06 +0700

    :smiley: Update portfolio

--- public/script.js
+++ public/script.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
                 $w				= $(window),
                 viewTop			= $w.scrollTop(),
                 viewBottom		= viewTop + $w.height(),
-                _top			= $t.offset().top,
+                _top			= $($(this).attr('context')).offset(),
                 _bottom			= _top + $t.height(),
                 compareTop		= partial === true ? _bottom : _top,
                 compareBottom	= partial === true ? _top : _bottom;
